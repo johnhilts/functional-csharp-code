1a755226dc1c3480b1b5f91ff947fd1ac3bba189
John Hilts <johnhilts@live.com>
2018-10-30 13:50:30 -0700

    formatting

--- Exercises/Chapter01/Exercises.cs
+++ Exercises/Chapter01/Exercises.cs
@@ -64,14 +64,14 @@ namespace Exercises.Chapter1
         }
 
 
-      // 3. Generalize your implementation to take a `List<T>`, and additionally a 
-      // `Comparison<T>` delegate.
+        // 3. Generalize your implementation to take a `List<T>`, and additionally a 
+        // `Comparison<T>` delegate.
 
-      // 4. In this chapter, you've seen a `Using` function that takes an `IDisposable`
-      // and a function of type `Func<TDisp, R>`. Write an overload of `Using` that
-      // takes a `Func<IDisposable>` as first
-      // parameter, instead of the `IDisposable`. (This can be used to fix warnings
-      // given by some code analysis tools about instantiating an `IDisposable` and
-      // not disposing it.)
-   }
+        // 4. In this chapter, you've seen a `Using` function that takes an `IDisposable`
+        // and a function of type `Func<TDisp, R>`. Write an overload of `Using` that
+        // takes a `Func<IDisposable>` as first
+        // parameter, instead of the `IDisposable`. (This can be used to fix warnings
+        // given by some code analysis tools about instantiating an `IDisposable` and
+        // not disposing it.)
+    }
 }
